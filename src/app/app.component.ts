07a42e34f453140fd14adc2e1c7c1c442d59e2fe
Tuan Tran Van <4468525+huntertran@users.noreply.github.com>
2022-04-09 19:08:37 +0000

    async pipe for rxjs and angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, Injectable, OnInit } from '@angular/core';
+import { Observable, of } from 'rxjs';
+
+@Injectable()
+export class Service {
+  // Cannot change the code in this class
+  public getRandom(): Observable<number> {
+    return of(Math.random());
+  }
+}
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'so-angular';
+export class AppComponent implements OnInit {
+
+  constructor(private service: Service) { }
+  public random$: Observable<number> = new Observable<number>();
+
+  ngOnInit(): void {
+    // this.random$ = this.service.getRandom();
+  }
+
+  buttonClick(): void {
+    // how can I update the random with this.service.getRandom()?
+    this.random$ = this.service.getRandom();
+    console.log('button clicked')
+  }
 }
